0ee2f085597250c223d7802838c6c65d2abcbfc7
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2026-09-17 01:45:42 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,22 @@ type Server struct {
 	db *sql.DB
 }
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	db, err := ConnectDB()
